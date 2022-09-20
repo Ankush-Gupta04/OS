68086856381e5818bc84b005c71b092c5133fea6
Ankush-Gupta04 <100823564+Ankush-Gupta04@users.noreply.github.com>
2022-09-20 14:19:10 +0530

    Add files via upload

---
+++ Assignment 0/test.asm
@@ -0,0 +1,59 @@
+segment .data
+	fmts: db "%s",0
+	fmtn: db "%d",0
+	msg2: db "Enter no",10,0
+	msg1: db "Enter string",10,0
+	msg: db "Hello",10,0
+
+
+segment .bss
+	string: resb 16
+	number: resb 4
+
+segment .text
+	extern printf
+	extern scanf
+	global main
+	default rel
+
+main:
+	push rbp
+
+	lea rdi,[fmts]
+	lea rsi,[msg]
+	mov rax,0
+	call printf
+
+	lea rdi,[fmts]
+	lea rsi,[msg1]
+	mov rax,0
+	call printf
+
+	lea rdi,[fmts]
+	lea rsi,[string]
+	mov rax,0
+	call scanf
+
+	lea rdi,[fmts]
+	lea rsi,[string]
+	mov rax,0
+	call printf
+
+	lea rdi,[fmts]
+	lea rsi,[msg2]
+	mov rax,0
+	call printf
+
+	lea rdi,[fmtn]
+	lea rsi,[number]
+	mov rax,0
+	call scanf
+
+	lea rdi,[fmtn]
+	mov rsi,[number]
+	mov rax,0
+	call printf
+
+	mov rax, 0
+	pop rbp
+	ret
