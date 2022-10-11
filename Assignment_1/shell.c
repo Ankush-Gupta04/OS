a3bfd589e1d46cd1ff50a53ba7f469e259cefbe1
Ankush-Gupta04 <100823564+Ankush-Gupta04@users.noreply.github.com>
2022-10-11 20:31:04 +0530

    Add files via upload

--- Assignment_1/shell.c
+++ Assignment_1/shell.c
@@ -32,17 +32,18 @@ int main(){
             token = strtok(NULL, " ");
         }
         // printf("%s",exection[0]);
-	int check  = 0;
-        if (strcmp(exection[0], "exit\n") == 0){
-		check = 1;
-            	break;
+	int check  = 0;else if (strcmpp(exection[0],"date") == 0 && strcmp(exection[1],"-I\n")){
+                check = 1;
+                strcat(rf,"/cat");
+                execv(rf,"-I");
+            }
         }
         else if (exection[0] == NULL){
             break;
         }
         else if (strcmp(exection[0], "cd\n") == 0){
             // printf("cd1");
-	    check = 1;
+	        check = 1;
             chdir("/home/ankush"); // {do /home/ankush}
             char add[100];
             getcwd(add,100);
@@ -50,8 +51,7 @@ int main(){
         }
         else if (strcmp(exection[0], "cd") == 0 && strcmp(exection[1], "..\n") == 0){
             // printf("cd2");
-	    check = 1;
-	
+	        check = 1;
             chdir("..");
             char add[100];
             getcwd(add,100);
@@ -59,49 +59,63 @@ int main(){
         }
         else if(strcmp(exection[0], "echo") == 0 && exection[1]==NULL){
             printf("A1>>>\n");
-	    check = 1;
+	        check = 1;
         }
         else if (strcmp(exection[0], "echo") == 0){
             int i=1;
-	    check = 1;
+	        check = 1;
             printf("A1>>>");
             while(exection[i+1]!= NULL){
                 printf("%s ",exection[i]);
                 i++;
             }
             printf("%s",exection[i]);
-            ///
         }
         else if (strcmp(exection[0], "pwd\n") == 0){
             // printf("pwd1");
             char add[100];
-	    check = 1;
+	        check = 1;
             getcwd(add,100);
             printf("A1>>>%s\n",add);
         }
         else if (strcmp(exection[0], "pwd") == 0 && strcmp(exection[1], "-L\n") == 0){
             // printf("pwd2");
             char add[100];
-	    check = 1;
+	        check = 1;
             getcwd(add,100);
             printf("A1>>>%s\n",add);
         }
-	if (fork() == 0){
-		if (strcmp(exection[0],"ls\n") == 0){
-			check = 1;
-			strcat(rf,"/ls");
-			execv(rf,null);	
-		}
-		else if (strcmp(exection[0],"ls") == 0 && strcmp(exection[1],"-R\n") == 0){
-			check = 1;
-			strcat(rf,"/ls");
-			execv(rf,"-R");
-		}
-		else if (strcmp(exection[0],"cat") == 0 || strcmp(exection[0] , "cat\n") == 0){
-			check = 1;
-			strcat(rf,"/cat");
-			execv(rf,exection[1],exection[2]);
-		}
-	}
+        if (fork() == 0){
+            if (strcmp(exection[0],"ls\n") == 0){
+                check = 1;
+                strcat(rf,"/ls");
+                execv(rf,null);	
+            }
+            else if (strcmp(exection[0],"ls") == 0 && strcmp(exection[1],"-R\n") == 0){
+                check = 1;
+                strcat(rf,"/ls");
+                execv(rf,"-R");
+            }
+            else if (strcmp(exection[0],"cat") == 0){
+                check = 1;
+                strcat(rf,"/cat");
+                if (strcmp(exection[1],"-n") == 0){
+                    execv(rf,exection[2],"-n");
+                }
+                else{
+                     execv(rf,exection[1],NULL);
+                }
+            }
+            else if (strcmpp(exection[0],"date") == 0 && strcmp(exection[1],"-I\n")){
+                check = 1;
+                strcat(rf,"/cat");
+                execv(rf,"-I");
+            }
+            else if (strcmpp(exection[0],"date") == 0 && strcmp(exection[1],"-R\n")){
+                check = 1;
+                strcat(rf,"/cat");
+                execv(rf,"-R");
+            }
+        }
     }
 }
