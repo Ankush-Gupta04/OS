5341419f13615678e11e22055b474b0005c507f4
Ankush-Gupta04 <100823564+Ankush-Gupta04@users.noreply.github.com>
2022-11-21 01:01:00 +0530

    Create bash2.sh

---
+++ A2/Q1/1.2/bash2.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+mkdir a2
+cd a2
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.19.8.tar.xz
+tar -xvJf linux-5.19.8.tar.xz
+cd linux-5.19.8
+wget https://raw.githubusercontent.com/gopi487krishna/ArtixConfig/main/config-rev-9-gold
+cp config-rev-9-gold .config
+make menuconfig 
+make -j4
