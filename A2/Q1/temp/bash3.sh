a8f7644c75546a299404b46b4332231bfe8cb80a
Ankush-Gupta04 <100823564+Ankush-Gupta04@users.noreply.github.com>
2022-11-21 14:57:08 +0530

    Create bash3.sh

---
+++ A2/Q1/temp/bash3.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+mkdir a3
+cd a3
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.19.8.tar.xz
+tar -xvJf linux-5.19.8.tar.xz
+cd linux-5.19.8
+wget https://raw.githubusercontent.com/gopi487krishna/ArtixConfig/main/config-rev-9-gold
+cp config-rev-9-gold .config
+make menuconfig 
+make -j4
+cd /home/ankush/os/temp/q1/final
+ls
+echo "deleting a3"
+rm -r a3
